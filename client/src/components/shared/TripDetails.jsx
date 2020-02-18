d7669cd6f0c4cf46c3e9c57faa655c9940a120ef
Tara Solbrig <tsolbrig87@gmail.com>
2020-02-18 16:17:08 -0500

    converted TripDetails to class

--- client/src/components/shared/TripDetails.jsx
+++ client/src/components/shared/TripDetails.jsx
@@ -1,27 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
-const TripDetails = (props) => {
-    return (
-        <div>
-            <h4>Depart at:</h4>
-            <h4>Arrive by:</h4>
+class TripDetails extends Component {
+    constructor(props) {
+        super(props)
 
-            <form>
-                <input type="radio" name="ticketType"></input>
-                <label for="one-way">One Way: $...</label>
-                <input type="radio" name="ticketType"></input>
-                <label for="round-trip">Round Trip: $...</label>  
-                <input type="radio" name="ticketType"></input>
-                <label for="weekly-pass">Weekly Pass: $</label>
-                <input type="radio" name="ticketType"></input>
-                <label for="monthly-pass">Monthly Pass: $</label>
-                <label for="accessible-trip">Accessible Trip</label>
-                <input type="radio" name="accessible"></input>
+        this.state = {
 
-                <input type="submit"></input>
-            </form>
-        </div>
-    )
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>Depart at:</h4>
+                <h4>Arrive by:</h4>
+
+                <form>
+                    <label>
+                        <input type="radio" name="ticketType" />
+                        <span>One Way: $</span>
+                    </label>
+
+                    <label>
+                        <input type="radio" name="ticketType" />
+                        <span>Round Trip: $...</span>
+                    </label>
+
+                    <label>
+                        <input type="radio" name="ticketType" />
+                        <span>Weekly Pass: $</span>
+                    </label>
+
+                    <label>
+                        <input type="radio" name="ticketType" />
+                        <span>Monthly Pass: $</span>
+                    </label>
+
+                    <label>
+                        <span>Accessible Trip</span>
+                        <input type="radio" name="accessible" />
+                    </label>
+
+                    <input type="submit"></input>
+                </form>
+
+                <div className='view-times'>
+                    <NavLink exact to='/schedule'>View all available times</NavLink>
+                </div>
+
+                <div>
+                    <Link exact to='/ticket'>Review Ticket</Link>
+                </div>
+
+
+            </div>
+        )
+    }
 }
 
 export default TripDetails
