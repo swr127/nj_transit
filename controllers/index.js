abd627f7b51686a572d679dce3b2858aa2a53037
swr127 <samantha.w.russell@gmail.com>
2020-02-18 10:59:52 -0500

    created routes, controllers, and server

---
+++ controllers/index.js
@@ -0,0 +1,108 @@
+const { Ticket } = require("../models")
+const { Route } = require("../models")
+const { Stop } = require("../models")
+const { Bus } = require("../models")
+
+const getAllTickets = async (req, res) => {
+    try {
+        const tickets = await Ticket.findAll()
+        return res.status(200).json({ tickets })
+    } catch (error) {
+        return res.status(500).send( error.message )
+    }
+}
+
+const getAllRoutes = async (req, res) => {
+    try {
+        const routes = await Route.findAll()
+        return res.status(200).json({ routes })
+    } catch (error) {
+        return res.status(500).send( error.message )
+    }
+}
+
+const getAllStops = async (req, res) => {
+    try {
+        const stops = await Stop.findAll()
+        return res.status(200).json({ stops })
+    } catch (error) {
+        return res.status(500).send( error.message )
+    }
+}
+
+const getAllBuses = async (req, res) => {
+    try {
+        const buses = await Bus.findAll()
+        return res.status(200).json({ buses })
+    } catch (error) {
+        return res.status(500).send( error.message )
+    }
+}
+
+const getBusById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const bus = await Bus.findOne({ 
+            where: { id: id }
+    })
+    if (bus) {
+        return res.status(200).json({ bus })
+    }
+        return res.status(404).send('Bus does not exist!')
+    } catch (error) {
+        return res.status(500).send( error.message )
+    }
+}
+
+const createBus = async (req, res) => {
+    try {
+        const bus = await Bus.create(req.body)
+        return res.status(201).json({ bus })
+    } catch (error) {
+        return res.status(500).send( error.message )
+    }
+}
+
+const updateBus = async (req, res) => {
+    try {
+        const { id } = req.params
+        const updated = await Bus.update(req.body, {
+            where: { id: id }
+    })
+    if (updated) {
+        const updatedBus = await Bus.findOne({
+            where: { id : id }
+        })
+        return res.status(201).json({ updatedBus })
+    }
+        throw new Error('Bus not found!')
+    } catch (error) {
+        return res.status(500).send( error.message )
+    }
+}
+
+const deleteBus = async (req, res) => {
+    try {
+        const { id } = req.params
+        const deleted = await Bus.destroy({
+            where: { id : id }
+    })
+    if (deleted) {
+        return res.status(204).send('Bus successfully deleted!')
+    }
+        throw new Error('Bus not found!')
+    } catch (error) {
+        return res.status(500).send( error.message )
+    }
+}
+
+module.exports = {
+    getAllTickets,
+    getAllRoutes,
+    getAllStops,
+    getAllBuses,
+    getBusById,
+    createBus,
+    updateBus,
+    deleteBus
+}
---
+++ routes/index.js
@@ -0,0 +1,30 @@
+const { Router } = require('express')
+const router = Router()
+const controllers = require('../controllers')
+
+// HIGH PRIORITY CRUD 
+router.get('/', (req, res) => res.send('Welcome to NJ Transit!'))
+router.get('/api/tickets', controllers.getAllTickets)
+router.get('/api/routes', controllers.getAllRoutes)
+router.get('/api/stops', controllers.getAllStops)
+router.get('/api/buses', controllers.getAllBuses)
+router.get('/api/buses/:id', controllers.getBusById)
+router.post('/api/buses', controllers.createBus)
+router.put('/api/buses/:id', controllers.updateBus)
+router.delete('/api/buses/:id', controllers.deleteBus)
+
+// LOW PRIORITY CRUD
+router.get('/api/tickets/:id', controllers.getTicketById)
+router.post('/api/tickets', controllers.createTicket)
+router.put('/api/tickets/:id', controllers.updateTicket)
+router.delete('/api/tickets/:id', controllers.deleteTicket)
+router.get('/api/routes/:id', controllers.getRouteById)
+router.post('/api/routes', controllers.createRoute)
+router.put('/api/routes/:id', controllers.updateRoute)
+router.delete('/api/routes/:id', controllers.deleteRoute)
+router.get('/api/stops/:id', controllers.getStopById)
+router.post('/api/stops', controllers.createStop)
+router.put('/api/stops/:id', controllers.updateStop)
+router.delete('/api/stops/:id', controllers.deleteStop)
+
+module.exports = router
