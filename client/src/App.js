49bc9f09a141c4be0bdde1b0592c3e59e384ef74
swr127 <samantha.w.russell@gmail.com>
2020-02-18 11:17:30 -0500

    created react router in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './components/routes/Home'
+import PlanATrip from './components/routes/PlanATrip'
+import Service from './components/routes/Service'
+import Schedule from './components/routes/Schedule'
+import Menu from './components/routes/Menu'
+import Ticket from './components/routes/Ticket'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
+      <React.Fragment>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/planatrip' component={PlanATrip} />
+          <Route exact path='/service' component={Service} />
+          <Route exact path='/schedule' component={Schedule} />
+          <Route exact path='/menu' component={Menu} />
+          <Route exact path='/ticket' component={Ticket} />
+        </Switch>
+      </React.Fragment>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
