d903a1d1e9a4f693c6c3b6a2155fa271051ef0eb
Tara Solbrig <tsolbrig87@gmail.com>
2020-02-18 15:53:58 -0500

    added radio inputs to TripDetails

--- client/src/components/shared/TripDetails.jsx
+++ client/src/components/shared/TripDetails.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const TripDetails = (props) => {
     return (
-        <h1>Trip Details</h1>
+        <div>
+            <h4>Depart at:</h4>
+            <h4>Arrive by:</h4>
+
+            <form>
+                <input type="radio" name="ticketType"></input>
+                <label for="one-way">One Way: $...</label>
+                <input type="radio" name="ticketType"></input>
+                <label for="round-trip">Round Trip: $...</label>  
+                <input type="radio" name="ticketType"></input>
+                <label for="weekly-pass">Weekly Pass: $</label>
+                <input type="radio" name="ticketType"></input>
+                <label for="monthly-pass">Monthly Pass: $</label>
+                <label for="accessible-trip">Accessible Trip</label>
+                <input type="radio" name="accessible"></input>
+
+                <input type="submit"></input>
+            </form>
+        </div>
     )
 }
 
