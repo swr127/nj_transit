bded542a70357555d00642989bb22eba803f9224
Tara Solbrig <tsolbrig87@gmail.com>
2020-02-18 11:57:44 -0500

    created boilerplate in PlanTrip and Ticket

---
+++ client/src/components/routes/Ticket.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import apiUrl from '../../apiConfig'
+import axios from 'axios'
+import Layout from '../shared/Layout'
+
+class Ticket extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            const response = await axios(`${apiUrl}/ticket`)
+            this.setState({ })
+        } catch (err) {
+            console.error(err)
+        }
+    }
+
+    render() {
+        return (
+            <Layout>
+                <h1>Hello from Ticket!</h1>
+            </Layout>
+        )
+    }
+}
+
+export default Ticket
