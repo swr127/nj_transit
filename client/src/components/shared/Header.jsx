0c3f9acb30bb620f41edb6536bdd1f892c022c91
Tara Solbrig <tsolbrig87@gmail.com>
2020-02-19 14:57:07 -0500

    added position style to header

--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import headerImage from '../../images/nj-transit-header.png'
+import '../../styles/header.css'
 
 const Header = (props) =>
 {
-    return (<Link to='/'><img src={headerImage} alt='NJ Transit Header Image' width='320' /></Link>)
+    return (<Link to='/'><img className='header-image' src={headerImage} alt='NJ Transit Header Image' width='320' /></Link>)
 }
 
 export default Header
