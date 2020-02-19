40e5093af028b1e69acef16f1db3cdc3d156288f
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-02-19 13:50:44 -0500

    working through menu route and service status shared components

--- client/src/components/routes/Menu.jsx
+++ client/src/components/routes/Menu.jsx
@@ -7,21 +7,42 @@ import arrowIcon from '../../images/arrow-icon.png'
 import planIcon from '../../images/plan-icon.png'
 import scheduleIcon from '../../images/schedule-icon.png'
 import busIcon from '../../images/bus-icon.png'
+import busIconLarge from '../../images/bus-icon-large.png'
 import lightRailIcon from '../../images/light-rail-icon.png'
 import railIcon from '../../images/rail-icon.png'
 import serviceIcon from '../../images/service-status-icon.png'
+
+import '../../styles/menu.css'
 
 const Menu = (props) =>
 {
     return (<Layout>
         <div>
-            <div><img src={railIcon} /><div>Rail</div></div>
-            <div><img src={lightRailIcon} /><div>L. Rail</div></div>
-            <div><img src={busIcon} /><div>Bus</div></div>
+            <div className='small'><img src={railIcon} /><div>Rail</div></div>
+            <div className='small'><img src={lightRailIcon} /><div>L. Rail</div></div>
+            <div><img src={busIconLarge} /><div>Bus</div></div>
             <div>
-                <Link to='/menu'><button><img src={planIcon} />Plan a trip<img src={arrowIcon} /></button></Link>
-                <Link to='/menu'><button><img src={scheduleIcon} />View all schedules<img src={arrowIcon} /></button></Link>
-                <Link to='/service'><button><img src={serviceIcon} />Service Status<img src={arrowIcon} /></button></Link>
+                <Link to='/menu'>
+                    <button className='home-button'>
+                        <img src={planIcon} />
+                        <span className='home-button-text'>Plan a trip</span>
+                        <img src={arrowIcon} />
+                    </button>
+                </Link>
+                <Link to='/menu'>
+                    <button className='home-button'>
+                        <img src={scheduleIcon} />
+                        <span className='home-button-text'>View all schedules</span>
+                        <img src={arrowIcon} />
+                    </button>
+                </Link>
+                <Link to='/service'>
+                    <button className='home-button'>
+                        <img src={serviceIcon} />
+                        <span className='home-button-text'>Service Status</span>
+                        <img src={arrowIcon} />
+                    </button>
+                </Link>
             </div>
             <ServiceStatus />
         </div>
