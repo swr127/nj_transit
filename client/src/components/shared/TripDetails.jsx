e0f0a83fa8482919e65fef3769cb1ae6ad39aa34
Tara Solbrig <tsolbrig87@gmail.com>
2020-02-20 10:51:48 -0500

    styled accessible trip line

--- client/src/components/shared/TripDetails.jsx
+++ client/src/components/shared/TripDetails.jsx
@@ -5,6 +5,7 @@ import apiUrl from '../../apiConfig'
 import '../../styles/tripdetails.css'
 import boardingIcon from '../../images/boarding-icon.png'
 import travelTimeIcon from '../../images/travel-time-icon.png'
+import bitmapIcon from '../../images/bitmap.png'
 
 // does this component still need to be a class? I think we're just passing props through at this point
 
@@ -51,36 +52,66 @@ class TripDetails extends Component {
 
 
                 <form className='ticket-grid'>
-                    {/* <label> */}
-                        <input className='checkbox' id='one-way' type="radio" name="ticketType" value="One Way: $13.50" onChange={this.props.handleTypeChange} />
-                        <span className='text' id='one-way-text'>One Way: $13.50</span>
-                    {/* </label> */}
+                    <input 
+                        className='checkbox' 
+                        id='one-way' 
+                        type="radio" 
+                        name="ticketType" 
+                        value="One Way: $13.50" 
+                        onChange={this.props.handleTypeChange} 
+                    />
+                    <span className='text' id='one-way-text'>
+                        One Way: $13.50
+                    </span>
 
-                    {/* <label> */}
-                        <input className='checkbox' id='round-trip' type="radio" name="ticketType" value="Round Trip: $25.50" onChange={this.props.handleTypeChange} />
-                        <span className='text' id='round-trip-text'>Round Trip: $25.50</span>
-                    {/* </label> */}
+                    <input 
+                        className='checkbox' 
+                        id='round-trip' 
+                        type="radio" 
+                        name="ticketType" 
+                        value="Round Trip: $25.50" 
+                        onChange={this.props.handleTypeChange} 
+                    />
+                    <span className='text' id='round-trip-text'>
+                        Round Trip: $25.50
+                    </span>
 
-                    {/* <label> */}
-                        <input className='checkbox' id='weekly' type="radio" name="ticketType" value="Weekly Pass: $70.50" onChange={this.props.handleTypeChange} />
-                        <span className='text' id='weekly-text'>Weekly Pass: $70.50</span>
-                    {/* </label> */}
+                    <input 
+                        className='checkbox' 
+                        id='weekly' 
+                        type="radio" 
+                        name="ticketType" 
+                        value="Weekly Pass: $70.50" 
+                        onChange={this.props.handleTypeChange} 
+                    />
+                    <span className='text' id='weekly-text'>
+                        Weekly Pass: $70.50
+                    </span>
 
-                    {/* <label> */}
-                        <input className='checkbox' id='monthly' type="radio" name="ticketType" value="Monthly Pass: $280" onChange={this.props.handleTypeChange} />
-                        <span className='text' id='monthly-text'>Monthly Pass: $280</span>
-                    {/* </label> */}
-
+                    <input 
+                        className='checkbox' 
+                        id='monthly' 
+                        type="radio" 
+                        name="ticketType" 
+                        value="Monthly Pass: $280" 
+                        onChange={this.props.handleTypeChange} 
+                    />
+                    <span className='text' id='monthly-text'>
+                        Monthly Pass: $280
+                    </span>
                 </form>
 
-                <form>
-                    {/* <label> */}
-                        <span>Accessible Trip</span>
-                        <input type="checkbox" name="accessible" />
-                    {/* </label> */}
+                <form className='accessible-grid'>
+                        <img className='accessible-image' src={bitmapIcon}></img>
+                        <span className='accessible-text'>
+                            Accessible Trip
+                        </span>
+                        <input 
+                            className='accessible-box' 
+                            type="checkbox" 
+                            name="accessible"
+                        />
                 </form>
-
-                {/* <Link exact to='/ticket'>Submit</Link> */}
 
                 <div className='view-times'>
                     <NavLink exact to='/schedule'>View all available times</NavLink>
