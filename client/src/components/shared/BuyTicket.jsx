7102aba762729bcc6a15d2c96d61a8a236ee55a5
Tara Solbrig <tsolbrig87@gmail.com>
2020-02-20 12:46:01 -0500

    styled stopping at line in buyticket

--- client/src/components/shared/BuyTicket.jsx
+++ client/src/components/shared/BuyTicket.jsx
@@ -1,40 +1,49 @@
 import React from 'react'
+import '../../styles/tripdetails.css'
+import '../../styles/buyticket.css'
+import boardingIcon from '../../images/boarding-icon.png'
+import travelTimeIcon from '../../images/travel-time-icon.png'
+import stoppingAtIcon from '../../images/stopping-at-icon@2x.png'
 
 const BuyTicket = (props) => {
     console.log(props)
     return (
         <div>
+            <div className='travel-grid'>
+                <img className='boarding-image' src={boardingIcon}></img>
+                <h5 className='boarding-on'>Boarding on:</h5>
+                <h4 className='platform'>Platform 2</h4>
 
-            <h5>Boarding on:</h5>
-            <h4>Platform 2</h4>
-
-            <h5>Travel Time:</h5>
-            <h4>7:15 AM - 8:20 AM</h4>
-
-
-            <h4>Stopping at:</h4>
+                <img className='travel-image' src={travelTimeIcon}></img>
+                <h5 className='travel-time'>Travel Time:</h5>
+                <h4 className='times'>
+                    <span className='time-span'>7:15 AM</span>
+                    —
+                    <span className='time-span'>8:20 AM</span>
+                </h4>
+            </div>
 
             <div className='stopping-at'>
-                <div className='row-1'>
-                    <div className='column-1'><h5>7:15 AM</h5></div>
-                    <div className='column-2'></div>
-                    <div className='column-3'><h4>Bus Stop 1</h4></div>
-                </div>
-                <div className='row-2'>
-                    <div className='column-1'></div>
-                    <div className='column-2'></div>
-                    <div className='column-3'><p>(Click to see more)</p></div>
-                </div>
-                <div className='row-3'>
-                    <div className='column-1'><h5>8:05 AM</h5></div>
-                    <div className='column-2'></div>
-                    <div className='column-3'><h4>Bus Stop 3</h4></div>
-                </div>
-                <div className='row-4'>
-                    <div className='column-1'><h5>8:20 AM</h5></div>
-                    <div className='column-2'></div>
-                    <div className='column-3'><h4>Bus Stop 4</h4></div>
-                </div>
+                <img className='stopping-image' src={stoppingAtIcon}></img>
+                <h4 className='stopping-text'>Stopping at:</h4>
+            </div>
+
+            <div className='stops'>
+                <div className='column-1'><h5>7:15 AM</h5></div>
+                <div className='column-2'></div>
+                <div className='column-3'><h4>Bus Stop 1</h4></div>
+
+                <div className='column-1'></div>
+                <div className='column-2'></div>
+                <div className='column-3'><p>(Click to see more)</p></div>
+
+                <div className='column-1'><h5>8:05 AM</h5></div>
+                <div className='column-2'></div>
+                <div className='column-3'><h4>Bus Stop 3</h4></div>
+
+                <div className='column-1'><h5>8:20 AM</h5></div>
+                <div className='column-2'></div>
+                <div className='column-3'><h4>Bus Stop 4</h4></div>
             </div>
 
             <label>
