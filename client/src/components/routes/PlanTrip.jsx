35b8a5535a32175d5022e247073e268e57ec4e8a
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2020-02-20 17:20:47 -0500

    working on conditinonal rendering in from/to

--- client/src/components/routes/PlanTrip.jsx
+++ client/src/components/routes/PlanTrip.jsx
@@ -5,31 +5,37 @@ import Layout from '../shared/Layout'
 import TripDetails from '../shared/TripDetails'
 import FromTo from '../shared/FromTo'
 
-class PlanTrip extends Component {
-    constructor(props) {
+class PlanTrip extends Component
+{
+    constructor(props)
+    {
         super(props)
 
         this.state = {
         }
     }
 
-    async componentDidMount() {
-        try {
+    async componentDidMount()
+    {
+        try
+        {
             const response = await axios(`${apiUrl}/planatrip`)
-            this.setState({ })
-        } catch (err) {
+            this.setState({})
+        } catch (err)
+        {
             console.error(err)
         }
     }
 
-    render() {
+    render()
+    {
         return (
             <Layout>
-                <FromTo 
-                    handleChangeFromField={this.props.handleChangeFromField} 
-                    handleChangeToField={this.props.handleChangeToField} 
+                <FromTo
+                    handleChangeFromField={this.props.handleChangeFromField}
+                    handleChangeToField={this.props.handleChangeToField}
                 />
-                <TripDetails 
+                <TripDetails
                     handleTypeChange={this.props.handleTypeChange}
                 />
             </Layout>
