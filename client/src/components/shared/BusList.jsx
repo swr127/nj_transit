fbeac8597bc4c27bf96b86719434e183a864ba9d
swr127 <samantha.w.russell@gmail.com>
2020-02-20 14:22:06 -0500

    text align left

--- client/src/components/shared/BusList.jsx
+++ client/src/components/shared/BusList.jsx
@@ -18,17 +18,17 @@ class BusList extends Component {
 
         if (this.props.routeId) {
             bus = this.props.routeId.data.route.Buses.map((bus, id) => (
-                <div className='bus-list' key={id}>
-                    <div className='bus-box'>
-                        <div className='bus-number'>Bus #{bus.id} </div>
-                        <div className='bus-middle'>
-                            <img className='bus-img' src={busLine} alt='Bus Line'/>
-                            <div className='bus-platform'>{bus.platform}</div>
-                            <img className='bus-status' src={goodService} alt='Service Status'/>
+                <div className='bus-list-list' key={id}>
+                    <div className='bus-list-box'>
+                        <div className='bus-list-number'>Bus #{bus.id} </div>
+                        <div className='bus-list-middle'>
+                            <img className='bus-list-img' src={busLine} alt='Bus Line'/>
+                            <div className='bus-list-platform'>{bus.platform}</div>
+                            <img className='bus-list-status' src={goodService} alt='Service Status'/>
                         </div>
-                        <div className='bus-times'>
-                            <span className='start-time'>{bus.departureTime, moment().format('LT')}</span>
-                            <span className='end-time'>{bus.arrivalTime, moment().format('LT')}</span>
+                        <div className='bus-list-times'>
+                            <span className='bust-start-time'>{bus.departureTime, moment().format('LT')}</span>
+                            <span className='bus-end-time'>{bus.arrivalTime, moment().format('LT')}</span>
                         </div>
                     </div>
                     {/* write code for status image */}
@@ -39,8 +39,8 @@ class BusList extends Component {
         return (
             <div>
                 <div className='bus-headers'> 
-                    <span className='platform'>Platform</span>
-                    <span className='status'>Status</span>
+                    <span className='bus-headers-platform'>Platform</span>
+                    <span className='bus-headers-status'>Status</span>
                 </div>
                 <div>{bus}</div>
             </div>
