6308d9396df93eee5867a4acf4bd453a76416cd5
Tara Solbrig <tsolbrig87@gmail.com>
2020-02-21 10:46:03 -0500

    popup styling complete

--- client/src/components/popups/BoughtTicket.jsx
+++ client/src/components/popups/BoughtTicket.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../../styles/boughtticket.css'
+import xIcon from '../../images/bitmap@2x.png'
 
 const BoughtTicket = (props) => {
     console.log(props)
@@ -11,9 +12,33 @@ const BoughtTicket = (props) => {
     return (
         <div className='ticket-modal'>
             <div className='ticket-modal-content'>
-                <span className='close-popup' onClick={handleClick}>&times;</span>
+                <span className='close-popup' onClick={handleClick}><img src={xIcon} alt='X button'></img></span>
+
                 <h1 className='great-trip-text'>Have A Great Trip!</h1>
                 <h5 className='question-text'>How would you like your ticket?</h5>
+
+                <form>
+                    <div className='get-ticket-buttons'>
+                        <input 
+                            className='ticket-checkbox' 
+                            type="radio" 
+                            name="get-ticket" 
+                            value="download" 
+                            checked
+                        />
+                        <h4 className='get-ticket-text'>Download ticket to my phone.</h4>
+                    </div>
+
+                    <div className='get-ticket-buttons'>
+                        <input 
+                            className='ticket-checkbox' 
+                            type="radio" 
+                            name="get-ticket" 
+                            value="email"
+                        />
+                        <h4 className='get-ticket-text'>Email my ticket to me.</h4>
+                    </div>
+                </form>
             </div>
         </div>
     )
